cb83071f1804af329af33a6acc87140137e0cbb6
rodrisanz <rodrigosanz64@gmail.com>
2020-04-18 14:00:34 -0300

    Mas del archivo de configuracion

--- Team/team.c
+++ Team/team.c
@@ -4,11 +4,19 @@ TEAMConfig* config;
 
 int main() {
 
+    initialize_structures();
     read_config_options();
 
 }
 
+void initialize_structures(){
+
+    config = malloc(sizeof(TEAMConfig));
+}
+
 void read_config_options() {
+
     t_config *config_file = config_create("../team.config");
+    config -> posiciones_entrenadores = config_get_array_value(config_file,"POSICIONES_ENTRENADORES");
 
 }

865c0e1df9bd19ac1b2fedb6e4169f06c0ac6f8e
emi <emiliano.ortiz@hotmail.com>
2020-06-03 16:29:03 -0300

    Montaje terminado

--- Gamecard/tallGrass.c
+++ Gamecard/tallGrass.c
@@ -4,19 +4,39 @@
 
 #include "tallGrass.h"
 
+
 int main(){
-    printf("salio todo : %d \n",montar(".."));
+    montar("..");
 }
 
+/* Crea la estrucutra de carpetas del file system siempre y cuando no exista
+ * Los errores los muestra imprimiendo en consola
+ * path: String con el punto donde vas a querer montarlo.
+ * return: 0 exito, 1 error
+ */
 int montar(char* punto_montaje){
-    crear_estructura_carpetas(punto_montaje);
+    //Creo el path del fileSystem
+    char* path_tall_grass = concatenar_strings(punto_montaje,"/Tall_Grass");
 
-    return 0;
+    limpiar_unidades_antiguas(path_tall_grass);
+
+    //Creo la carpeta donde va a estar el fileSystem
+    int resultado_tall_grass = crear_carpeta(path_tall_grass , ACCESSPERMS);
+
+    if(resultado_tall_grass == 0){
+        //Creo las carpetas que lo componen y devuelvo el resultado
+        int resultado_met = crear_metadata( path_tall_grass);
+        int resultado_blok = crear_blocks(path_tall_grass);
+        int resultado_file = crear_file(path_tall_grass);
+        return (resultado_met && resultado_blok && resultado_file);
+    }
+
+    return 1;
 }
 
 
 /*Verifica si existe la carpeta tall Grass
- * return: -1 si existe
+ * return: 1 si existe
  * return: 0 si no existe
  */
 void limpiar_unidades_antiguas(char* path){
@@ -46,57 +66,70 @@ void limpiar_unidades_antiguas(char* path){
     }
 }
 
-/* Crea la estrucutra de carpetas del file system siempre y cuando no exista
- * Los errores los muestra imprimiendo en consola
- * path: String con el punto donde vas a querer montarlo.
- * return: 0 exito, 1 error
- */
-int crear_estructura_carpetas(char* punto_montaje){
-    //Creo la carpeta principal
-
-    char* path_tall_grass = concatenar_strings(punto_montaje,"/Tall_Grass");
-
-    limpiar_unidades_antiguas(path_tall_grass);
-
-    int resultado_tall_grass = crear_carpeta(path_tall_grass , ACCESSPERMS);
-
-    char* path_file = concatenar_strings(path_tall_grass,"/Files");
-
-    int resultado_file = crear_carpeta(path_file,ACCESSPERMS);
-
-    crear_metadata( path_tall_grass);
-
-    char* path_blocks = concatenar_strings(path_tall_grass,"/Blocks");
-
-    int resultado_bloques = crear_carpeta(path_blocks, ACCESSPERMS);
-
-    return resultado_tall_grass && resultado_bloques &&resultado_file;
-}
-
+//Crea la estructura de metadata del fileSystem
 int crear_metadata(char* path){
     char* path_metadata = concatenar_strings(path,"/Metadata");
 
+    //Creo la carpeta
     int resultado = crear_carpeta(path_metadata, ACCESSPERMS);
 
+    //Verifico error
     if(resultado==0){
+        //Creo el archivo metadata.bin
         char* path_metadata_bin = concatenar_strings(path_metadata,"/Metadata.bin");
         FILE * archivo_metadata = fopen(path_metadata_bin,"w+");
         fprintf(archivo_metadata,"BLOCK_SIZE=%d\nBLOCKS=%d\nMAGIC_NUMBER=%s",BLOCK_SIZE, BLOCKS, MAGIC_NUMBER);
         fclose(archivo_metadata);
         free(path_metadata_bin);
 
+        //Creo el archivo bitmap.bin
         char* path_bitmap_bin = concatenar_strings(path_metadata,"/Bitmap.bin");
         FILE * archivo_bitmap = fopen(path_bitmap_bin,"w+");
         t_bitarray* bitmap = create_bitmap(BLOCKS);
-        fprintf(archivo_bitmap,"%s",bitmap->bitarray);
+        escribir_bitmap(bitmap, archivo_bitmap);
+        bitarray_destroy(bitmap);
         fclose(archivo_bitmap);
         free(path_bitmap_bin);
+
+        return 0;
     }
+    return 1;
 }
 
+//Crea la estructura blocks del fileSyste
+int crear_blocks(char* path){
+    char* path_blocks = concatenar_strings(path,"/Blocks");
+
+    int resultado = crear_carpeta(path_blocks, ACCESSPERMS);
+
+    if(resultado == 0){
+        //Creo los bloques del file system
+        for(int i = 1; i <= BLOCKS; i++){
+            //Genero path para el bloque que voy a crear
+            char* nombre_bloque = string_from_format("/%d.bin", i);
+            char* path_bloque =  concatenar_strings(path_blocks,nombre_bloque);
+
+            FILE * archivo_metadata = fopen(path_bloque,"w+");
+            fclose(archivo_metadata);
+
+            free(nombre_bloque);
+            free(path_bloque);
+        }
+        return 0;
+    }
+    return 1;
+}
+
+//Crea la estructura file del filesystem
+int crear_file(char* path){
+
+    char* path_file = concatenar_strings(path,"/Files");
+
+    return crear_carpeta(path_file,ACCESSPERMS);
+}
 
 /*Sacada de lo mas profundo de starckoverflow
- *path: direccion de la carpeta
+ * Param: direccion de la carpeta
  * Return 0 por exito
  * Return -1 por error
  */
@@ -144,6 +177,10 @@ int remove_directory(const char *path) {
     return r;
 }
 
+/* Concatena dos string y devuelve otro sin necesidad de reservar
+ * memoria de antemano
+ * Parmas: strings a concatenar
+ */
 char* concatenar_strings(char*path1, char* path2){
     char* concatenado = malloc(strlen(path1) + strlen(path2) + 1);
     strcpy(concatenado, path1);
@@ -152,18 +189,96 @@ char* concatenar_strings(char*path1, char* path2){
     return concatenado;
 }
 
+/* Crea una carpeta real
+ * Param: El path donde queres que se cree
+ * Param: El modo en el que lo queres crear
+ */
+
 int crear_carpeta(char* path, int modo){
     int resultado = mkdir(path , modo);
 
     if(resultado == -1){
         printf("Algo salio mal! %s\n", hstrerror(errno));
+        return 1;
     }
+
+    return 0;
 }
+
+/*
+ * Funciones de bitmap
+ */
 
 t_bitarray* create_bitmap(int cantidad_bloques){
 
-    int bitmap_size = cantidad_bloques/8;
-    char* bitmap_string = malloc(bitmap_size);
+    int bitmap_size = tamanio_bitmap(cantidad_bloques);
 
-    return bitarray_create(bitmap_string, bitmap_size);
+    char* bitmap_string = malloc(bitmap_size);
+    t_bitarray * bitarray = bitarray_create(bitmap_string, bitmap_size);
+
+    //Inicializo el bitmap en 0s
+    limpiar_bitmpa(bitarray);
+
+    return bitarray;
+}
+
+int tamanio_bitmap(int cantidad_bloques){
+
+    int bitmap_size = cantidad_bloques/8;
+
+    //Se fija si sobran bloques en el bitmap
+    if((cantidad_bloques%8)!=0){
+
+        //Si sobran le suma uno para agregarlo
+        bitmap_size++;
+    }
+
+    return bitmap_size;
+}
+
+void doom_bitmap(t_bitarray* bitarray){
+    //Imprimo cada bit
+    for(int i = 0; i < bitarray->size*8; i++){
+
+        //cada vez que se escriben 8bits hago un salto
+        if((i%8)==0){
+            printf("\n");
+        }
+        printf("%d", bitarray_test_bit(bitarray, i));
+    }
+    printf("\n");
+}
+
+void limpiar_bitmpa(t_bitarray* bitarray){
+
+    for(int i = 0; i < bitarray->size*8; i++){
+        bitarray_clean_bit(bitarray, i);
+    }
+}
+
+char* obtener_bitmap(FILE* archivo_bitmap){
+    //Pongo el puntero del archivo en 0
+    rewind(archivo_bitmap);
+
+    int bitmap_size = tamanio_bitmap(BLOCKS);
+    char* buffer = malloc(bitmap_size + 1);
+
+    //Copio cada byte del archivo a un buffer
+    for(int i = 0; i < bitmap_size;i++){
+        buffer[i] = fgetc(archivo_bitmap);
+    }
+
+    //Agrego el caracter de final de string
+    buffer[bitmap_size] = '\0';
+
+    return buffer;
+}
+
+void escribir_bitmap(t_bitarray* bitmap, FILE* archivo){
+
+    char* buffer = bitmap->bitarray;
+    for(int i = 0; i < bitmap->size;i++){
+        //copio cada byte al archivo
+        fputc(buffer[i],archivo);
+    }
 }
--- Gamecard/tallGrass.h
+++ Gamecard/tallGrass.h
@@ -5,7 +5,7 @@
 #ifndef GAMECARD_TALLGRASS_H
 #define GAMECARD_TALLGRASS_H
 #define BLOCK_SIZE 5
-#define BLOCKS 5192
+#define BLOCKS 64
 #define MAGIC_NUMBER "TALL_GRASS"
 
 #include <sys/types.h>
@@ -18,9 +18,16 @@
 #endif //GAMECARD_TALLGRASS_H
 
 int montar(char* punto_montaje);
-int crear_estructura_carpetas(char* punto_montaje);
+void limpiar_unidades_antiguas(char* path);
+int crear_metadata(char* path);
+int crear_blocks(char* path);
+int crear_file(char* path);
+int crear_carpeta(char* path, int modo);
 int remove_directory(const char *path);
 char* concatenar_strings(char*path1, char* path2);
-int crear_carpeta(char* path, int modo);
-int crear_metadata(char* path);
 t_bitarray* create_bitmap(int cantidad_bloques);
+void doom_bitmap(t_bitarray* bitarray);
+void limpiar_bitmpa(t_bitarray* bitarray);
+int tamanio_bitmap(int cantidad_bloques);
+char* obtener_bitmap(FILE* archivo_bitmap);
+void escribir_bitmap(t_bitarray* bitmap, FILE* archivo);

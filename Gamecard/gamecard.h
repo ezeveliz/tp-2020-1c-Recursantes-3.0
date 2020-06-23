a994bb4b680cc5d6566a7ab539114bd790202c34
emi <emiliano.ortiz@hotmail.com>
2020-06-23 14:03:09 -0300

    Conecciones andando, gracias eze por tanto.

--- Gamecard/gamecard.h
+++ Gamecard/gamecard.h
@@ -35,3 +35,25 @@ void mensaje_new_pokemon();
 void mensaje_get_pokemon();
 
 void mensaje_catch_pokemon();
+
+//------- funciones de conexion--------//
+
+int conectar_broker();
+
+void desconectar_broker(int broker_socket);
+
+void subscribe_to_queues();
+
+void* subscribe_to_queue_thread(void* arg);
+
+int connect_and_subscribe(MessageType cola);
+
+int connect_to_broker();
+
+void disconnect_from_broker(int broker_socket);
+
+bool subscribe_to_queue(int broker, MessageType cola);
+
+int initialize_server();
+
+void* server_function_gamecard(void* arg);

699f455697c9c58de74e65af8dc5ec5b9d47f827
emi <emiliano.ortiz@hotmail.com>
2020-07-17 19:33:51 -0300

    Arme los build de gameboy y gamecard

--- Gameboy/build_gameboy.sh
+++ Gameboy/build_gameboy.sh
@@ -5,6 +5,62 @@
 # Buscanos en redes sociales
 #
 
-#Compilo
-cmake CMakeLists.txt
+# Compilo
+#cmake CMakeLists.txt
 make
+
+#Asigno una variable con el path del archivo de configuracion
+CONFIG="gameboy_config"
+
+
+# Datos del Broker
+echo "Ingrese la ip del Broker"
+	echo -n "> "
+	read IP_BROKER
+
+echo "IP_BROKER=$IP_BROKER" > "$CONFIG"
+
+echo "Ingrese el puerto del Broker"
+	echo -n "> "
+	read PUERTO_BROKER
+
+echo "PUERTO_BROKER=$PUERTO_BROKER" >> "$CONFIG"
+
+
+# Datos proceso tema
+echo "Ingrese la ip del Team"
+	echo -n "> "
+	read IP_TEAM
+
+echo "IP_TEAM=$IP_TEAM" >> "$CONFIG"
+
+echo "Ingrese el puerto del Team"
+	echo -n "> "
+	read PUERTO_TEAM
+
+echo "PUERTO_TEAM=$PUERTO_TEAM" >> "$CONFIG"
+
+
+#Datos proceso gamecard
+
+IP_GAMECARD
+
+echo "Ingrese la ip del Gamecard"
+	echo -n "> "
+	read IP_GAMECARD
+
+echo "IP_GAMECARD=$IP_GAMECARD" >> "$CONFIG"
+
+echo "Ingrese el puerto del Gamecard"
+	echo -n "> "
+	read PUERTO_GAMECARD
+
+echo "PUERTO_GAMECARD=$PUERTO_GAMECARD" >> "$CONFIG"
+
+
+
+
+
+
+	
+	

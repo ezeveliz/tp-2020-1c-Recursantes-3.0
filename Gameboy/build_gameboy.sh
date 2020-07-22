8a14f2b5357dbcbc213a5ea363f0860083127a8e
emiortiz <38117993+emiortiz@users.noreply.github.com>
2020-07-22 16:08:30 -0300

    Arregle el build del gameboy para q sea mas rapido

--- Gameboy/build_gameboy.sh
+++ Gameboy/build_gameboy.sh
@@ -5,6 +5,17 @@
 # Buscanos en redes sociales
 #
 
+echo "  ██████╗  █████╗ ███╗   ███╗███████╗██████╗  ██████╗ ██╗   ██╗";
+echo " ██╔════╝ ██╔══██╗████╗ ████║██╔════╝██╔══██╗██╔═══██╗╚██╗ ██╔╝";
+echo " ██║  ███╗███████║██╔████╔██║█████╗  ██████╔╝██║   ██║ ╚████╔╝ ";
+echo " ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  ██╔══██╗██║   ██║  ╚██╔╝  ";
+echo " ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗██████╔╝╚██████╔╝   ██║   ";
+echo "  ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═════╝  ╚═════╝    ╚═╝   ";
+echo "                                                              ";
+echo "                                         By Recursantes 3.0   ";
+echo "                                                              ";
+
+
 #Bajo las pruebas 
 git clone https://github.com/sisoputnfrba/delibird-pruebas.git
 cd delibird-pruebas
@@ -16,36 +27,25 @@ make
 #Asigno una variable con el path del archivo de configuracion
 CONFIG="gameboy_config"
 
+#Datos fijos
+PUERTO_BROKER="5002"
+PUERTO_TEAM="5003"
+PUERTO_GAMECARD="5004"
+MAC="15"
 
-# Datos del Broker
+
+# Datos obligatorios
 echo "Ingrese la ip del Broker"
 	echo -n "> "
 	read IP_BROKER
 
 echo "IP_BROKER=$IP_BROKER" > "$CONFIG"
 
-echo "Ingrese el puerto del Broker"
-	echo -n "> "
-	read PUERTO_BROKER
-
-echo "PUERTO_BROKER=$PUERTO_BROKER" >> "$CONFIG"
-
-
-# Datos proceso tema
 echo "Ingrese la ip del Team"
 	echo -n "> "
 	read IP_TEAM
 
 echo "IP_TEAM=$IP_TEAM" >> "$CONFIG"
-
-echo "Ingrese el puerto del Team"
-	echo -n "> "
-	read PUERTO_TEAM
-
-echo "PUERTO_TEAM=$PUERTO_TEAM" >> "$CONFIG"
-
-
-#Datos proceso gamecard
 
 echo "Ingrese la ip del Gamecard"
 	echo -n "> "
@@ -53,20 +53,30 @@ echo "Ingrese la ip del Gamecard"
 
 echo "IP_GAMECARD=$IP_GAMECARD" >> "$CONFIG"
 
-echo "Ingrese el puerto del Gamecard"
-	echo -n "> "
-	read PUERTO_GAMECARD
+# Datos proceso tema
+if [ "$1" = "-p" ]
+then
+  echo "Ingrese el puerto del Broker"
+    echo -n "> "
+    read PUERTO_BROKER
 
+  echo "Ingrese el puerto del Team"
+    echo -n "> "
+    read PUERTO_TEAM
+
+  echo "Ingrese el puerto del Gamecard"
+    echo -n "> "
+    read PUERTO_GAMECARD
+
+  echo "Ingrese el id del proceso"
+    echo -n "> "
+    read MAC
+fi
+
+
+echo "PUERTO_BROKER=$PUERTO_BROKER" >> "$CONFIG"
+echo "PUERTO_TEAM=$PUERTO_TEAM" >> "$CONFIG"
 echo "PUERTO_GAMECARD=$PUERTO_GAMECARD" >> "$CONFIG"
-
-#Datos propios
-echo "Ingrese el id del proceso"
-	echo -n "> "
-	read MAC
-
 echo "MAC=$MAC" >> "$CONFIG"
-
-
-
 	
 	

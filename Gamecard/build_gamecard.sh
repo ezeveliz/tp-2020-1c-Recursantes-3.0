a1a2cb9893b3261da181ee0df67e49645d1a8000
emi <emiliano.ortiz@hotmail.com>
2020-07-22 15:53:53 -0300

    Subo builder del gamecard

--- Gamecard/build_gamecard.sh
+++ Gamecard/build_gamecard.sh
@@ -5,6 +5,21 @@
 # Buscanos en redes sociales
 #
 
+
+
+echo "   ██████╗  █████╗ ███╗   ███╗███████╗ ██████╗ █████╗ ██████╗ ██████╗ ";
+echo "  ██╔════╝ ██╔══██╗████╗ ████║██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗";
+echo "  ██║  ███╗███████║██╔████╔██║█████╗  ██║     ███████║██████╔╝██║  ██║";
+echo "  ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  ██║     ██╔══██║██╔══██╗██║  ██║";
+echo "  ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗╚██████╗██║  ██║██║  ██║██████╔╝";
+echo "   ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ";
+echo "                                                                            ";
+echo "                                                         By Recursantes 3.0";
+echo "                                                                     ";
+echo "                                                                     ";
+
+
+
 # Compilo
 cmake CMakeLists.txt
 make
@@ -20,48 +35,77 @@ echo "Ingrese la ip del Broker"
 
 echo "IP_BROKER=$IP_BROKER" > "$CONFIG"
 
-echo "Ingrese el puerto del Broker"
-	echo -n "> "
-	read PUERTO_BROKER
+#Variables 
+PUERTO_BROKER="5002"
+PUNTO_MONTAJE_TALLGRASS="."
+TIEMPO_RETARDO_OPERACION="5"
+TIEMPO_DE_REINTENTO_OPERACION="5"
+TIEMPO_DE_REINTENTO_CONEXION="10"
+MAC="92"
+PUERTO_GAMECARD="5004"
 
-echo "PUERTO_BROKER=$PUERTO_BROKER" >> "$CONFIG"
 
-# Datos de operacion
-echo "Ingrese punto de montaje"
-	echo -n "> "
-	read PUNTO_MONTAJE_TALLGRASS
+# Configuracion personalizada 
+if [ "$1" = "-p" ]
+then 
+	echo "Ingrese el puerto del Broker"
+		echo -n "> "
+		read PUERTO_BROKER
 
-echo "PUNTO_MONTAJE_TALLGRASS=$PUNTO_MONTAJE_TALLGRASS" >> "$CONFIG"
+	echo "PUERTO_BROKER=$PUERTO_BROKER" >> "$CONFIG"
 
-echo "Ingrese tiempo de retardo de operacion"
-	echo -n "> "
-	read TIEMPO_RETARDO_OPERACION
+	# Datos de operacion
+	echo "Ingrese punto de montaje"
+		echo -n "> "
+		read PUNTO_MONTAJE_TALLGRASS
 
-echo "TIEMPO_RETARDO_OPERACION=$TIEMPO_RETARDO_OPERACION" >> "$CONFIG"
+	echo "PUNTO_MONTAJE_TALLGRASS=$PUNTO_MONTAJE_TALLGRASS" >> "$CONFIG"
 
-echo "Ingrese tiempo de reintento de operacion"
-	echo -n "> "
-	read TIEMPO_DE_REINTENTO_OPERACION
+	echo "Ingrese tiempo de retardo de operacion"
+		echo -n "> "
+		read TIEMPO_RETARDO_OPERACION
 
-echo "TIEMPO_DE_REINTENTO_OPERACION=$TIEMPO_DE_REINTENTO_OPERACION" >> "$CONFIG"
+	echo "TIEMPO_RETARDO_OPERACION=$TIEMPO_RETARDO_OPERACION" >> "$CONFIG"
 
-echo "Ingrese tiempo de reintento de conexion"
-	echo -n "> "
-	read TIEMPO_DE_REINTENTO_CONEXION
+	echo "Ingrese tiempo de reintento de operacion"
+		echo -n "> "
+		read TIEMPO_DE_REINTENTO_OPERACION
 
-echo "TIEMPO_DE_REINTENTO_CONEXION=$TIEMPO_DE_REINTENTO_CONEXION" >> "$CONFIG"
+	echo "TIEMPO_DE_REINTENTO_OPERACION=$TIEMPO_DE_REINTENTO_OPERACION" >> "$CONFIG"
 
-echo "Ingrese ID del proceso"
-	echo -n "> "
-	read MAC
+	echo "Ingrese tiempo de reintento de conexion"
+		echo -n "> "
+		read TIEMPO_DE_REINTENTO_CONEXION
 
-echo "MAC=$MAC" >> "$CONFIG"
+	echo "TIEMPO_DE_REINTENTO_CONEXION=$TIEMPO_DE_REINTENTO_CONEXION" >> "$CONFIG"
 
-echo "Ingrese el puerto de gamecard"
-	echo -n "> "
-	read PUERTO_GAMECARD
+	echo "Ingrese ID del proceso"
+		echo -n "> "
+		read MAC
 
-echo "PUERTO_GAMECARD=$PUERTO_GAMECARD" >> "$CONFIG"
+	echo "MAC=$MAC" >> "$CONFIG"
+
+	echo "Ingrese el puerto de gamecard"
+		echo -n "> "
+		read PUERTO_GAMECARD
+
+	echo "PUERTO_GAMECARD=$PUERTO_GAMECARD" >> "$CONFIG"
+else
+
+	echo "PUERTO_BROKER=$PUERTO_BROKER" >> "$CONFIG"
+
+	echo "PUNTO_MONTAJE_TALLGRASS=$PUNTO_MONTAJE_TALLGRASS" >> "$CONFIG"
+
+	echo "TIEMPO_RETARDO_OPERACION=$TIEMPO_RETARDO_OPERACION" >> "$CONFIG"
+
+	echo "TIEMPO_DE_REINTENTO_OPERACION=$TIEMPO_DE_REINTENTO_OPERACION" >> "$CONFIG"
+
+	echo "TIEMPO_DE_REINTENTO_CONEXION=$TIEMPO_DE_REINTENTO_CONEXION" >> "$CONFIG"
+
+	echo "MAC=$MAC" >> "$CONFIG"
+
+	echo "PUERTO_GAMECARD=$PUERTO_GAMECARD" >> "$CONFIG"
+fi
 
 #./gamecard
 
